5dfce23dd9fa0a02071d5f95aa6c1b356708036d
Bogdan-work <kalganbogdan@gmail.com>
2023-11-19 11:59:34 +0100

    fix burger and adaptive flower

--- burger.js
+++ burger.js
@@ -3,12 +3,14 @@ const burgerWindow = document.querySelector(".header-burger_active");
 const title = document.querySelector(".title-first");
 const descr = document.getElementById("hero-descr");
 const close = document.querySelector(".header-burger_active-close");
+const model = document.querySelector(".model");
 
 burger.onclick = () => {
   burgerWindow.style.display = "flex";
   burger.style.display = "none";
   title.style.display = "none";
   descr.style.display = "none";
+  model.style.display = "none";
 };
 
 close.onclick = () => {
@@ -16,7 +18,12 @@ close.onclick = () => {
   burger.style.display = "flex";
   title.style.display = "block";
   descr.style.display = "block";
+  model.style.display = "block";
 };
+
+if (close.onclick.window.innerWidth <= 480) {
+  model.style.display = "none";
+}
 
 if (window.innerWidth > 1200) {
   burger.style.display = "none";
